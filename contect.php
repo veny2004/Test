6f39d695085beb3777e420b17e699755b5214230
Veny2004 <sivanesanelaveny@gmail.com>
2025-09-28 15:49:39 +0530

    first commit

---
+++ contect.php
@@ -0,0 +1,20 @@
+<html>
+    <body> 
+        <?php
+         echo "CONTECT US";
+         ?>
+         <form>
+            <lable for ="NAME">NAME</lable><br>
+            <input type ="text" id="NAME" NAME="NAME"><br><br>
+            <lable for ="EMAIL">EMAIL</lable><br>
+            <input type ="EMAIL" id="EMAIL" NAME="EMAIL"><br><br>
+            <lable for ="MESSAGE">MESSAGE</lable><br>
+            <textarea id="MASSAGE"NAME="MASSAGE"></textarea><br><br>
+            <input type ="SUBMIT" value="SUBMIT"><br>
+         </form>
+          
+    </body> 
+</hlml>
+
+
+    
